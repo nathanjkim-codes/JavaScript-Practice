9f167ab8da40055a5a1c54cc58a3323bd2b42d31
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-11-09 17:49:43 -0800

    feat: add blue <h3> element to container

--- DOM_add_elements_js/script.js
+++ DOM_add_elements_js/script.js
@@ -1,8 +1,14 @@
-// select the container
+// Select the container
 const container = document.querySelector("#container");
 
-//create a red <p> and add it
+// Create a red <p> and add it
 const redP = document.createElement("P");
 redP.textContent = "Hey I'm red!";
 redP.style.color = "red";
 container.appendChild(redP);
+
+// Create a blue <h3> and add it
+const blueH3 = document.createElement("h3");
+blueH3.textContent = "I'm blue h3!";
+blueH3.style.color = "blue";
+container.appendChild(blueH3);
