f5da5f0fadd28303d8a40fce5e39d4e5011d309b
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-11-10 10:00:34 -0800

    feat: Create the input and button

---
+++ DOM_dynamic_list_creator/script.js
@@ -0,0 +1,10 @@
+// Select the container where everything will go
+const container = document.querySelector("#container");
+
+// Create the input and button
+const input = document.createElement("input");
+input.type = "text";
+input.placeholder = "Enter an item";
+
+const addButton = document.createElement("button");
+addButton.textContent = "Add Item";
