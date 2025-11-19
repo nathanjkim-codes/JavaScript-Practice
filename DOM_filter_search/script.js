56f9924642ccbd90798c1de941382ff6ebcc4ff3
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2025-11-18 22:44:51 -0800

    feat: initialize members array and select DOM elements

---
+++ DOM_filter_search/script.js
@@ -0,0 +1,6 @@
+const members = [];
+
+const cardContainer = document.getElementById("cardContainer");
+const nameInput = document.getElementById("nameInput");
+const addBtn = document.getElementById("addBtn");
+const searchInput = document.getElementById("searchInput");
