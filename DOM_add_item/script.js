aebae14701b2f7576609f0be63f5fd5330dd34dc
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2026-01-29 22:41:31 -0800

    feat: add addItem function with input validation

--- DOM_add_item/script.js
+++ DOM_add_item/script.js
@@ -2,3 +2,9 @@
 const nameInput = document.getElementById("nameInput");
 const addBtn = document.getElementById("addBtn");
 const nameList = document.getElementById("nameList");
+
+// Function to add item
+function addItem() {
+  const name = nameInput.value.trim(); // remove whitespace
+
+  if (name === "") return; // return if input is empty
