c3a1a71349874052c95b554aacf6bc524f3b4d1d
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2026-01-30 21:14:34 -0800

    feat: create and append list item from input value

--- DOM_add_item/script.js
+++ DOM_add_item/script.js
@@ -8,4 +8,11 @@ function addItem() {
   const name = nameInput.value.trim(); // remove whitespace
 
   if (name === "") return; // return if input is empty
+
+  // Create a new list item element
+  const li = document.createElement("li");
+  li.textContent = name;
+
+  nameList.appendChild(li);
+  nameInput.value = "";
 }
