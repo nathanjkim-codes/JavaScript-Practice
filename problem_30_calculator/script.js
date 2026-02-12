cac687a885d2bb6dae6df58dea0a12a049c008fd
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2026-02-12 14:12:44 -0800

    feat: implement add function

---
+++ problem_30_calculator/script.js
@@ -0,0 +1,24 @@
+const add = function (a, b) {
+  return a + b;
+};
+console.log(add(3, 5)); // 8
+
+const subtract = function () {};
+
+const sum = function () {};
+
+const multiply = function () {};
+
+const power = function () {};
+
+const factorial = function () {};
+
+// Do not edit below this line
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
